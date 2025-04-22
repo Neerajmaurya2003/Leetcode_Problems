34d420356641fa13548efaf9fab5fa46ff9c1c8b
Neerajmaurya2003 <152865835+Neerajmaurya2003@users.noreply.github.com>
2025-04-22 21:07:58 +0530

    Add files via upload

---
+++ 12. Integer To Roman/Integer_To_Roman.kt
@@ -0,0 +1,31 @@
+class Solution {
+    fun intToRoman(num: Int): String {
+
+        val map = mapOf(
+            1000 to "M",
+            900 to "CM",
+            500 to "D",
+            400 to "CD",
+            100 to "C",
+            90 to "XC",
+            50 to "L",
+            40 to "XL",
+            10 to "X",
+            9 to "IX",
+            5 to "V",
+            4 to "IV",
+            1 to "I"
+        )
+        var s=StringBuilder()
+        var nums=num
+
+        for((value,symbol) in map){
+            while(nums>=value){
+                s.append(symbol)
+                nums -=value
+            }
+        }
+
+        return s.toString()
+    }
+}
